f15682dd53730f5e645e4bd3b6af93d342dfea7b
Zachary <zpgeary@gmail.com>
2026-03-04 14:18:24 -0500

    added to written section

--- week7/FDE.jl
+++ week7/FDE.jl
@@ -23,5 +23,6 @@ println("Average Temperature of active sensors above 70°F: ", Average_temp)
 
 # This code is safer than the imperative version because no data is mutated. 
 # In other words at no stage in the pipeline is any of the original data modified. 
-# Instead the data is copied modified and placed into a new container which is then passed to the next step.
-# This allows for easy parallelization when additional threads are available. 
+# Instead the data is copied, modified and placed into a new container which is then passed to the next step.
+# This allows for easy parallelization when additional threads are available because each thread is free to access 
+# the data available to it without working about race conditions. 
